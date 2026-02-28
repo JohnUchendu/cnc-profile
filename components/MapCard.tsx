c74b5bfbfc6b691456abdce67e58e26272c68819
MACBOOK PRO <johnchnd195@gmail.com>
2026-02-28 12:15:13 +0100

    map

--- components/MapCard.tsx
+++ components/MapCard.tsx
@@ -3,8 +3,7 @@ export default function MapCard() {
     <div className="mapCard">
       <h2>Location</h2>
       <iframe
-        src="https://www.google.com/maps?q=128+City+Road,+London,+EC1V+2NX,+United+Kingdom&output=embed"
-        allowFullScreen
+       src="https://www.google.com/maps?q=20+Farrington+St,+London,+EC4A+4AB,+United+Kingdom&output=embed"allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
         title="20 Farrington St, London EC4A4AB, UK"
